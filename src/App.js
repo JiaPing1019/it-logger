29f3913b8b2c9f51ddcf3ad34612bbc8862d3af4
JiaPing <jumpping4@gmail.com>
2019-12-29 22:16:43 +0900

    Redux Store & Provider && Logs Reducer, Actions & Types

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import AddLogModal from './components/Logs/AddLogModal';
 import EditLogModal from './components/Logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
 import TechListModal from './components/techs/TechListModal';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -17,17 +19,21 @@ const App = () => {
     M.AutoInit();
   });
 
-  return <Fragment>
-    <SearchBar />
-    <div className="container">
-      <AddBtn />
-      <AddLogModal />
-      <EditLogModal />
-      <AddTechModal />
-      <Logs />
-      <TechListModal />
-    </div>
-  </Fragment>;
+  return (
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <Logs />
+          <TechListModal />
+        </div>
+      </Fragment>
+    </Provider>
+  );
 }
 
 export default App;
